9058747ce327254cf4ece77dbf117ee6b533306b
Sebastian Loncar <sebastian.loncar@gmail.com>
2022-04-14 14:40:28 +0200

    experiment

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownRenderChild } from "obsidian";
+import { WidgetType } from "@codemirror/view"
+import { EditorView, Decoration } from "@codemirror/view"
+import { ViewUpdate, ViewPlugin, DecorationSet } from "@codemirror/view"
+import { Extension } from "@codemirror/state"
+import * as CodeMirror from 'codemirror';
 
 // Remember to rename these classes and interfaces!
 
@@ -11,71 +17,56 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 }
 
 export default class MyPlugin extends Plugin {
+	private editorExtension: Extension[] = [];
 	settings: MyPluginSettings;
 
+	updateEditorExtension() {
+		// Empty the array while keeping the same reference
+		// (Don't create a new array here)
+		this.editorExtension.length = 0;
+	
+		// Create new editor extension
+		let myNewExtension = this.createEditorExtension();
+		// Add it to the array
+		this.editorExtension.push(myNewExtension);
+	
+		// Flush the changes to all editors
+		this.app.workspace.updateOptions();
+	}
+
 	async onload() {
-		await this.loadSettings();
+		this.registerEditorExtension(this.editorExtension);
+		// this.registerCodeMirror((cm) => {
+		// 	console.log("reg CM");
+		// 	const pos = cm.getCursor();
+		// 	cm.addWidget(pos, document.createElement("div"), false);
+		// 	// cm.sta
+		// });
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+
+		
+		this.registerMarkdownPostProcessor((element, context) => {
+			// const codeblocks = element.querySelectorAll("code");
+
+			// for (let index = 0; index < codeblocks.length; index++) {
+			//   const codeblock = codeblocks.item(index);
+			//   const text = codeblock.innerText.trim();
+			//   const isEmoji = text[0] === ":" && text[text.length - 1] === ":";
+
+			//   if (isEmoji) {
+			// 	context.addChild(new Emoji(codeblock, text));
+			//   }
+			// }
+
+			console.log(element);
+
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,18 +82,41 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
+export class Emoji extends MarkdownRenderChild {
+	static ALL_EMOJIS: Record<string, string> = {
+		":+1:": "👍",
+		":sunglasses:": "😎",
+		":smile:": "😄",
+	};
+
+	text: string;
+
+	constructor(containerEl: HTMLElement, text: string) {
+		super(containerEl);
+
+		this.text = text;
+	}
+
+	onload() {
+		const emojiEl = this.containerEl.createSpan({
+			text: Emoji.ALL_EMOJIS[this.text] ?? this.text,
+		});
+		this.containerEl.replaceWith(emojiEl);
+	}
+}
+
 class SampleModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,11 +130,11 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
 
 		new Setting(containerEl)
 			.setName('Setting #1')
@@ -135,3 +149,75 @@ class SampleSettingTab extends PluginSettingTab {
 				}));
 	}
 }
+
+class CheckboxWidget extends WidgetType {
+	constructor(readonly checked: boolean) { super() }
+
+	eq(other: CheckboxWidget) { return other.checked == this.checked }
+
+	toDOM() {
+		let wrap = document.createElement("span")
+		wrap.setAttribute("aria-hidden", "true")
+		wrap.className = "cm-boolean-toggle"
+		let box = wrap.appendChild(document.createElement("input"))
+		box.type = "checkbox"
+		box.checked = this.checked
+		return wrap
+	}
+
+	ignoreEvent() { return false }
+}
+
+
+function checkboxes(view: EditorView) {
+	let widgets = []
+	for (let { from, to } of view.visibleRanges) {
+		// if (type.name == "BooleanLiteral") {
+		let isTrue = view.state.doc.sliceString(from, to) == "true"
+		let deco = Decoration.widget({
+			widget: new CheckboxWidget(isTrue),
+			side: 1
+		})
+		widgets.push(deco.range(to))
+		// }
+	}
+	return Decoration.set(widgets)
+}
+
+const checkboxPlugin = ViewPlugin.fromClass(class {
+	decorations: DecorationSet
+
+	constructor(view: EditorView) {
+		this.decorations = checkboxes(view)
+	}
+
+	update(update: ViewUpdate) {
+		if (update.docChanged || update.viewportChanged)
+			this.decorations = checkboxes(update.view)
+	}
+}, {
+	decorations: v => v.decorations,
+
+	eventHandlers: {
+		mousedown: (e, view) => {
+			let target = e.target as HTMLElement
+			if (target.nodeName == "INPUT" &&
+				target.parentElement!.classList.contains("cm-boolean-toggle"))
+				return toggleBoolean(view, view.posAtDOM(target))
+		}
+	}
+})
+
+function toggleBoolean(view: EditorView, pos: number) {
+	let before = view.state.doc.sliceString(Math.max(0, pos - 5), pos)
+	let change
+	if (before == "false")
+		change = { from: pos - 5, to: pos, insert: "true" }
+	else if (before.endsWith("true"))
+		change = { from: pos - 4, to: pos, insert: "false" }
+	else
+		return false
+	view.dispatch({ changes: change })
+	return true
+}
+
